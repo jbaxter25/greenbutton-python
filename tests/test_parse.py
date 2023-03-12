705ed6688332ea07ed5d0230d6f9a90829c0fb20
Jordan Baxter <j.bax080@gmail.com>
2023-03-12 10:35:18 -0700

    Added test for parse feed.

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,3 +1,7 @@
+from greenbutton.parse import parse_feed
+
+
 def test_parse_yields_expected_results():
+    parse_feed("C:\Users\jdbax\projects\my-greenbutton-python\tests\testdata\PacificPower_GreenButton_03042023.xml")
 
     pass

746df2c27bf19b331dca61d2e5f5c4c108235011
Jordan Baxter <j.bax080@gmail.com>
2023-03-12 11:15:14 -0700

    Found out that green button data from my utility is wrong :/

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -3,31 +3,7 @@ from greenbutton.parse import parse_feed
 
 def test_parse_yields_expected_results():
     ups = parse_feed(
-        "C:/Users/jdbax/projects/my-greenbutton-python/tests/testdata/PacificPower_GreenButton_03122023.xml"
+        "C:/Users/jdbax/projects/my-greenbutton-python/tests/testdata/cc_customer_11.xml"
     )
-    for up in ups:
-        print("UsagePoint (%s) %s %s:" % (up.title, up.serviceCategory.name, up.status))
-        for mr in up.meterReadings:
-            print("  Meter Reading (%s) %s:" % (mr.title, mr.readingType.uom.name))
-            for ir in mr.intervalReadings:
-                print(
-                    "    %s, %s: %s %s"
-                    % (
-                        ir.timePeriod.start,
-                        ir.timePeriod.duration,
-                        ir.value,
-                        ir.value_symbol,
-                    ),
-                    end="",
-                )
-                if ir.cost is not None:
-                    print(" (%s%s)" % (ir.cost_symbol, ir.cost), end="")
-                if len(ir.readingQualities) > 0:
-                    print(
-                        "[%s]"
-                        % ", ".join([rq.quality.name for rq in ir.readingQualities]),
-                        end="",
-                    )
-                print("")
 
     pass
